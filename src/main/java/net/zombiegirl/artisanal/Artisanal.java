354a66d918e7023b15b60504bb7a8ddf35a40af7
ZombieGirlCoding <ryanle619@gmail.com>
2026-01-03 18:54:46 -0800

    reverted back to working code

--- src/main/java/net/zombiegirl/artisanal/Artisanal.java
+++ src/main/java/net/zombiegirl/artisanal/Artisanal.java
@@ -1,31 +1,20 @@
 package net.zombiegirl.artisanal;
 
 import net.fabricmc.api.ModInitializer;
-
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
-import net.minecraft.client.util.ModelIdentifier;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.WindChargeEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
 import net.zombiegirl.artisanal.event.GreatSwordBlockHandler;
 import net.zombiegirl.artisanal.item.ModItemGroups;
 import net.zombiegirl.artisanal.item.ModItems;
-import net.zombiegirl.artisanal.item.custom.GreatSwordItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Artisanal implements ModInitializer {
     public static final String MOD_ID = "artisanal";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static Identifier MOD_ID(String s) {
+        return null;
+    }
 
 
     @Override
