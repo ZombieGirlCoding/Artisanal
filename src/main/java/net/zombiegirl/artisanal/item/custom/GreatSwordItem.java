bbd74ced8e47c95cd900053f20666c530486144d
ZombieGirlCoding <ryanle619@gmail.com>
2026-01-04 20:58:48 -0800

    made the gui texture different from the handheld texture through mixins changed gui texture adjusted handheld texture to look better in first person

--- src/main/java/net/zombiegirl/artisanal/item/custom/GreatSwordItem.java
+++ src/main/java/net/zombiegirl/artisanal/item/custom/GreatSwordItem.java
@@ -11,7 +11,7 @@ import net.minecraft.world.World;
 public class GreatSwordItem extends SwordItem {
 
     public GreatSwordItem() {
-        super (ToolMaterials.NETHERITE, new Item.Settings().fireproof().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.NETHERITE, 6, -3.2F))
+        super (ToolMaterials.NETHERITE, new Item.Settings().fireproof().attributeModifiers(SwordItem.createAttributeModifiers(ToolMaterials.NETHERITE, 8, -3.2F))
         );
     }
 
